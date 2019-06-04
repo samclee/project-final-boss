17cb91d294ec49a05c0ad962e16a2c2849fc9872
samclee <samclee@gmail.com>
2019-06-03 17:04:33 -0700

    Player health indicator and boss health meter

---
+++ project-final-boss/Assets/Scripts/BossHealthBarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthBarController : MonoBehaviour
+{
+    public RectTransform healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setHP(0.40f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setHP(float f)
+    {
+
+
+        healthbar.localScale = new Vector3(f, 1f, 1f);
+    }
+}
---
+++ project-final-boss/Assets/Scripts/PlayerDataUIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDataUIController : MonoBehaviour
+{
+    public Text hpText;
+    private string hpTextPrefix = "HP: ";
+    private string spdTextPrefix = "SPD: ";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setHP(45);
+    }
+
+    public void setHP(int hp)
+    {
+        hpText.text = hpTextPrefix + hp.ToString() + "%";
+    }
+}
